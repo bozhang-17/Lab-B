81e0e5c57c5675708e5a40f362e693ca6b42be07
bozhang-17 <159536035+bozhang-17@users.noreply.github.com>
2024-02-23 09:22:28 +0000

    Add files via upload

---
+++ task_b.c
@@ -0,0 +1,20 @@
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
+#define SQUARE(n) ((n) * (n))
+int main(void){
+    double height= 0;
+	double volume = 0;
+	printf("Enter height: ");  
+	scanf("%lf", &height);  //input the height
+	height=SQUARE(height);
+	height=sqrt(height);
+	double pi = M_PI ;
+	double r=2.2; //radius is 2.2
+    double A=SQUARE(r)*pi ;
+	volume=A*height; //to caculate the volume
+	
+
+	printf("Volume: %.2f", volume); //output the volume
+	return 0;
+}
